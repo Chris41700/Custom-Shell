bff6cee2a82c796b0a7a7cfbcf8eb9d2fa9ccc0f
EstimadaE <estimadaelijah@gmail.com>
2022-11-29 18:00:22 -0500

    commits

--- tree.c
+++ tree.c
@@ -7,27 +7,27 @@
 
 void tree(){
     int fid = fork();
-    if(fid==0){ // Fork a child 
-        mkdir("dir0",0777); // Make dir0 with all access
-        exit(0); // Terminates child
+    if(fid==0){ // If fork is successful make the directory
+        mkdir("dir0",0777); // Make dir0 
+        exit(0); // Terminates child fork
     }
-    else if(fid<0){ // Prints error message if fork failed
+    else if(fid<0){ // If fork is not successfull print fork failed.
         printf("fork failed");
     }
     else {
-        wait(NULL);	// wait for child to complete
-    	chdir("dir0"); // change directory internally
+        wait(NULL);	// waits for child fork to complete
+    	chdir("dir0"); // change directory to dir0
         FILE *t1 = fopen("t1.txt","wb"); // Create t1.txt
         fclose(t1);
         FILE *t2 = fopen("t2.txt","wb"); // Create t2.txt
         fclose(t2);
         FILE *t3 = fopen("t3.txt","wb"); // Create t3.txt
         fclose(t3);
-        mkdir("dir1",0777); // Make dir1 with all access
+        mkdir("dir1",0777); // Make empty dir1
 	}
 }
 
-int main()
+int main() // test function
 {
     tree();
     return 0;
