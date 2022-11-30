2d26ec8a76c3b20b0efc79ff9660deb74f6787f9
Chris41700 <ChrisHui417@gmail.com>
2022-11-29 19:35:05 -0500

    tree works

--- tree.c
+++ tree.c
@@ -3,57 +3,63 @@
 
 void run_tree()
 {
-    // Fork a process
-    int fid = fork();
+    int check;
+    char *dirname = "dir0";
 
-    // Child Process
-    if (fid == 0)
-    {
-        printf("Making directory dir0\n");
+    // Make directory dir0
+    check = mkdir(dirname, 0777);
 
-        // Make dir0
-        mkdir("dir0", 0777);
-    }
-    // Fork was unsuccessful
-    if (fid < 0)
+    // Create directory was successful
+    if (!check)
     {
-        perror("Forked Failed\n");
-        exit(1);
+        printf("Directory dir0 was created\n");
     }
-    // Parent Process
+    // Failed to create directory
     else
     {
-        // Wait for child process to terminate
-        wait(NULL);
+        perror("Unable to create directory\n");
+        exit(1);
+    }
 
-        printf("Change directory to dir0\n");
+    printf("Change directory to dir0\n");
 
-        // Change directory to dir0
-        chdir("dir0");
+    // Change directory to dir0
+    chdir("dir0");
 
-        printf("Creating t1.txt, t2.txt, t3.txt\n");
+    printf("Creating t1.txt, t2.txt, t3.txt\n");
 
-        // Create t1.txt
-        FILE *t1 = fopen("t1.txt", "wb");
+    // Create t1.txt
+    FILE *t1 = fopen("t1.txt", "wb");
 
-        // Close the file
-        fclose(t1);
+    // Close the file
+    fclose(t1);
 
-        // Create t2.txt
-        FILE *t2 = fopen("t2.txt", "wb"); // Create t2.txt
+    // Create t2.txt
+    FILE *t2 = fopen("t2.txt", "wb"); // Create t2.txt
 
-        // Close the file
-        fclose(t2);
+    // Close the file
+    fclose(t2);
 
-        // Create t3.txt
-        FILE *t3 = fopen("t3.txt", "wb"); // Create t3.txt
+    // Create t3.txt
+    FILE *t3 = fopen("t3.txt", "wb"); // Create t3.txt
 
-        // Close the file
-        fclose(t3);
+    // Close the file
+    fclose(t3);
 
-        printf("Make directory dir1\n");
+    dirname = "dir1";
 
-        // Make empty dir1
-        mkdir("dir1", 0777);
+    // Make empty dir1
+    check = mkdir(dirname, 0777);
+
+    // Create directory was successful
+    if (!check)
+    {
+        printf("Directory dir1 was created\n");
+    }
+    // Failed to create directory
+    else
+    {
+        perror("Unable to create directory\n");
+        exit(1);
     }
 }
